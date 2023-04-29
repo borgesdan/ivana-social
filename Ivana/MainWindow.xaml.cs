f4f4bd6cc67a07b2a0dc003b14e2c802fa6a2dc7
borgesdan <borges_santos89@hotmail.com>
2023-04-29 17:09:09 -0300

    Implementa rotina de backup

--- Ivana/MainWindow.xaml.cs
+++ Ivana/MainWindow.xaml.cs
@@ -1,9 +1,13 @@
 ﻿using Ivana.Controls;
 using Ivana.Core;
+using Ivana.Data.Context;
 using Ivana.Data.Entities;
 using Ivana.Pages;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Win32;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -98,6 +102,72 @@ namespace Ivana
 
             MainContent.SelectedItem = persitedTab;
             persitedTab = null;
+        }
+
+        private void Backup_Click(object sender, RoutedEventArgs e)
+        {
+            var file = new SaveFileDialog();
+            file.DefaultExt = "db";
+            file.Filter = "Arquivo de Backup (*.db)|*.db";            
+
+            var resultDialog = file.ShowDialog();
+
+            if (!resultDialog.HasValue || !resultDialog.Value)
+                return;
+
+            var sourcePath = System.IO.Path.Combine(Environment.CurrentDirectory, "database.db");
+            var destinationPath = file.FileName;
+
+            try
+            {
+                if (File.Exists(sourcePath))
+                {
+                    File.Copy(sourcePath, destinationPath, true);
+                    MessageBox.Show("O arquivo de backup foi gerado com sucesso!", "Sucesso!", MessageBoxButton.OK, MessageBoxImage.Information);
+                }
+
+            }
+            catch
+            {
+                MessageBox.Show("Ocorreu um erro ao realizar o backup do arquivo. Tente salvar em outro lugar e se persistir o problema entre em contato com o administrador.",
+                    "Ocorreu um erro!", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private void Restore_Click(object sender, RoutedEventArgs e)
+        {
+            var messageResult = MessageBox.Show("Deseja realmente restaurar o backup? As informações atuais serão sobrescritas.", "Aviso", MessageBoxButton.YesNo, MessageBoxImage.Question);
+
+            if (messageResult != MessageBoxResult.Yes)
+                return;
+
+            var file = new OpenFileDialog();
+            file.DefaultExt = "db";
+            file.Filter = "Arquivo de Backup (*.db)|*.db";
+
+            var resultDialog = file.ShowDialog();
+
+            if (!resultDialog.HasValue || !resultDialog.Value)
+                return;
+
+            var sourcePath = file.FileName;
+            var destinationPath = System.IO.Path.Combine(Environment.CurrentDirectory, "database.db"); 
+
+            try
+            {
+                if (File.Exists(destinationPath))
+                {
+                    var context = AppDbContextFactory.Create();
+                    context.Database.EnsureDeleted();
+                }                
+
+                File.Copy(sourcePath, destinationPath, true);
+            }
+            catch
+            {
+                MessageBox.Show("Ocorreu um erro ao realizar a restauraração do backup. Se persistir o problema entre em contato com o administrador.",
+                    "Ocorreu um erro!", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
         }
     }
 }
